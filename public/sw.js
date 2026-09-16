af339b83130e5c6b98a610e646dbb200be649bd9
Ivo van Ham <ivo.van.ham@gmail.com>
2026-09-16 11:04:49 +0200

    PNG-iconen voor app-installatie

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Offline-cache. Vite geeft assets een hash, dus de app-shell wordt per deploy vervangen.
-const CACHE='scouting-shell-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.json','/icon.svg'])).then(()=>self.skipWaiting()))});
+const CACHE='scouting-shell-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.json','/icon.svg','/icon-192.png','/icon-512.png'])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
